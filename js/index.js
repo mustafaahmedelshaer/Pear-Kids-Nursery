743ca1088255ca550fdc0a2eb93489affc51cc01
Mustafa  Ahmed <74098034+mustafaahmedelshaer@users.noreply.github.com>
2021-06-24 17:40:28 +0200

    5.40
    5.40

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+  new WOW().init();
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 200,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop:true,
+    autoplay: {
+        delay: 700,
+        disableOnInteraction: false,
+      },
+   
+  });
+
